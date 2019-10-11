1b8841212f779ff7ca9bf217ab4de8256cf5f9b3
Your Name <dlinkoln99@gmail.com>
2019-10-11 11:51:35 +0300

    fix functionalty after Igor,add some fix related with write in DB

--- app.php
+++ app.php
@@ -3,20 +3,58 @@
         header('Content-Type: application/json');  
         require_once "connection.php";
         if($_SERVER['REQUEST_METHOD']=="GET"){
-            $sql = 'SELECT * FROM students';
+            $sql = 'SELECT * FROM studentsv1';
             $stmt=$pdo->prepare($sql);
             $stmt->execute();
             $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            // $sortStudent = 'SELECT * FROM studentsv1 WHERE '
+            
+            // foreach( $rows as $key){
+            //     foreach($key as $value){
+            //         if(){
+                        
+            //         }
+            //     }
+            // }
+            // var_dump($rows);
+            //var_dump($rows);
+            // function callSortTop($re){
+            //     if(is_string($re)||$re<89){
+            //         return false;
+            //     }
+            //     else{
+            //         return true;
+            //     }
+            //     // foreach($re as $key){
+            //     //     if(is_string($key) || $key<89 ){
+            //     //         return false;
+            //     //     }
+            //     //     else{
+            //     //         return true;
+            //     //     }
+            //     // }
+            // }
+            // function sortStudentSuccess($students){
+            //     foreach((array)$students as $key => $value ){
+            //         //var_dump($value);
+            //         $result = array_filter($value,"callSortTop");
+            //         var_dump($result);
+            //     }
+            // }
+            // sortStudentSuccess($rows);
+
             echo json_encode($rows);
             http_response_code(200);
         }
         elseif($_SERVER['REQUEST_METHOD']=="POST"){
             $entityBody = file_get_contents('php://input');
             $decodeBody = json_decode($entityBody);
-            var_dump($decodeBody);
-            $progress = ($decodeBody->math + $decodeBody->phis + $decodeBody->prog + $decodeBody->physical)/(count((array)$decodeBody) - 1); 
-            echo $response = json_encode("Student:$decodeBody->name created");
-            $sql = "INSERT INTO students(student_name,math,phis,prog,physical,progress) 
+            // var_dump($decodeBody);
+            $progress = ($decodeBody->math + $decodeBody->phis + $decodeBody->prog + $decodeBody->phisical)/(count((array)$decodeBody) - 1);
+            $response =  "Student:$decodeBody->name created";
+            echo $response = json_encode($response);
+            $sql = "INSERT INTO studentsv1(student_name,math,phis,prog,phisical,progress) 
             VALUES (:sname,:math,:phis,:prog,:physical,:progress)";
             $stmt=$pdo->prepare($sql);
             $lol = 43;
@@ -25,7 +63,7 @@
                 'math' => $decodeBody->math,
                 'phis' => $decodeBody->phis,
                 'prog' => $decodeBody->prog,
-                'physical' => $decodeBody->physical,
+                'physical' => $decodeBody->phisical,
                 'progress' => $progress,
             ]);
         }
